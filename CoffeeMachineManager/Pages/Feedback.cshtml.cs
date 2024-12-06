3670ed9f9ba9a169d5b39c5a16e9c5b4b807ec10
Amir Jawad <amirjawad79@gmail.com>
2024-12-06 15:34:22 +0100

    Updates to the system
    The buttons will now navigate users to their respective pages.

---
+++ CoffeeMachineManager/Pages/Feedback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeMachineManager.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Logic to fetch and display feedback
+        }
+    }
+}
---
+++ CoffeeMachineManager/Pages/Reports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeMachineManager.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Logic to fetch and display reports
+        }
+    }
+}
