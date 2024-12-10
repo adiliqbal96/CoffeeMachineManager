fab2666d91cbf01ee8cf115b793dabe72e8514bf
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-12-10 09:25:37 +0100

    Password hashing also implemented for editing users.

--- CoffeeMachineManager/Pages/User/Edit.cshtml.cs
+++ CoffeeMachineManager/Pages/User/Edit.cshtml.cs
@@ -14,10 +14,12 @@ namespace CoffeeMachineManager.Pages
     public class EditModel : PageModel
     {
         private readonly CoffeeMachineManager.Data.ApplicationDbContext _context;
+        private readonly CoffeeMachineManager.Interfaces.IPasswordHasher _passwordHasher;
 
-        public EditModel(CoffeeMachineManager.Data.ApplicationDbContext context)
+        public EditModel(CoffeeMachineManager.Data.ApplicationDbContext context, Interfaces.IPasswordHasher passwordHasher)
         {
             _context = context;
+            _passwordHasher = passwordHasher;
         }
 
         [BindProperty]
@@ -49,6 +51,8 @@ namespace CoffeeMachineManager.Pages
             }
 
             _context.Attach(User).State = EntityState.Modified;
+
+            User.Password = _passwordHasher.GetHash(User.Password ?? throw new NullReferenceException("User password input is null!"));
 
             try
             {
