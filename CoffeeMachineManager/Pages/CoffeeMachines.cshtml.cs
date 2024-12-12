2915636e1317ab5223b0d04b9e76e35dc264d82e
Amir Jawad <amirjawad79@gmail.com>
2024-12-12 17:38:54 +0100

    Reverted the CoffeeMachine to a simple template due to an error that will be addressed later.

--- CoffeeMachineManager/Pages/CoffeeMachines.cshtml.cs
+++ CoffeeMachineManager/Pages/CoffeeMachines.cshtml.cs
@@ -1,25 +1,12 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using CoffeeMachineManager.Data;
-using CoffeeMachineManager.Models;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CoffeeMachineManager.Pages
 {
     public class CoffeeMachinesModel : PageModel
     {
-        private readonly ApplicationDbContext _context;
-
-        public CoffeeMachinesModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public List<CoffeeMachine> CoffeeMachines { get; set; } = new List<CoffeeMachine>();
-
         public void OnGet()
         {
-            CoffeeMachines = _context.CoffeeMachines.ToList();
+            // Basic logic (if any) for the page.
         }
     }
 }
