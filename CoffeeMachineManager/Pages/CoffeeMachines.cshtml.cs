794451dbb579b8d14ed006d3bd1665ed0971cdb1
Amir Jawad <amirjawad79@gmail.com>
2024-12-12 19:43:25 +0100

    Added new SQL file for CoffeeMachine. Updated CoffeeMachine cshtml and code-behind file to enable coffee machine registration

--- CoffeeMachineManager/Pages/CoffeeMachines.cshtml.cs
+++ CoffeeMachineManager/Pages/CoffeeMachines.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoffeeMachineManager.Data;
+using CoffeeMachineManager.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoffeeMachineManager.Pages
 {
     public class CoffeeMachinesModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public CoffeeMachinesModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // List of Coffee Machines to display
+        public List<CoffeeMachine> CoffeeMachines { get; set; } = new List<CoffeeMachine>();
+
         public void OnGet()
         {
-            // Basic logic (if any) for the page.
+            // Fetch all coffee machines from the database
+            CoffeeMachines = _context.CoffeeMachines.ToList();
         }
     }
 }
