ad68d0f84857f17e4ab64d8b17b2612939e735e8
adiliqbal96 <adiliqbal9606@gmail.com>
2024-12-13 03:35:30 +0100

    Added dropdown menu with options for add coffee machine form.

--- CoffeeMachineManager/Pages/CreateCoffeeMachine.cshtml.cs
+++ CoffeeMachineManager/Pages/CreateCoffeeMachine.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CoffeeMachineManager.Data;
 using CoffeeMachineManager.Models;
+using System.Collections.Generic;
 
 namespace CoffeeMachineManager.Pages
 {
@@ -17,13 +18,32 @@ namespace CoffeeMachineManager.Pages
         [BindProperty]
         public CoffeeMachine CoffeeMachine { get; set; } = new CoffeeMachine();
 
+        // Dropdown options for location and type
+        public List<string> Locations { get; set; } = new List<string>
+        {
+            "Lobby",
+            "Cafeteria",
+            "Breakroom",
+            "Reception"
+        };
+
+        public List<string> Types { get; set; } = new List<string>
+        {
+            "Espresso Machine",
+            "Drip Coffee Maker",
+            "Pod Coffee Machine",
+            "Bean-to-Cup Machine"
+        };
+
         public IActionResult OnPost()
         {
+            // Validate form inputs
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            // Save coffee machine to the database
             _context.CoffeeMachines.Add(CoffeeMachine);
             _context.SaveChanges();
 
