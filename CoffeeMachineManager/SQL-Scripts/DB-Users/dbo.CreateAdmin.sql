6a500584391b702009174a84bf6cfeb8f90fb538
Frederik Christian Klee <frederikchrklee@gmail.com>
2024-12-13 10:28:19 +0100

    Expanding roles and ensuring rights are granted on the proper tables.

--- CoffeeMachineManager/SQL-Scripts/DB-Users/dbo.CreateAdmin.sql
+++ CoffeeMachineManager/SQL-Scripts/DB-Users/dbo.CreateAdmin.sql
@@ -10,7 +10,7 @@ USE CoffeeMachineManager;
 
 CREATE ROLE dbadmin;
 GRANT SELECT ON Users TO dbadmin;
-GRANT EXECUTE TO dbadmin;
-GRANT INSERT TO dbadmin;
+GRANT EXECUTE ON Users TO dbadmin;
+GRANT INSERT ON Users TO dbadmin;
 CREATE USER camilla FOR LOGIN camilla;
 ALTER ROLE dbadmin ADD MEMBER camilla;
