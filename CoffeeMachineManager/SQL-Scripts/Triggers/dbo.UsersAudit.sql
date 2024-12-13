a22b106a45bc1bd4bac5fc5e5b497fda38c0a3ca
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-12-13 07:32:05 +0100

    Operation types are now recorded as well when changes are made to the Users table. 'Delete' operation won't be recorded for some reason.

--- CoffeeMachineManager/SQL-Scripts/Triggers/dbo.UsersAudit.sql
+++ CoffeeMachineManager/SQL-Scripts/Triggers/dbo.UsersAudit.sql
@@ -4,14 +4,25 @@ GO
 -- Code mainly used from: https://www.mssqltips.com/sqlservertip/4055/create-a-simple-sql-server-trigger-to-build-an-audit-trail/
 
 CREATE TRIGGER TR_Users_Audit ON dbo.Users
-AFTER UPDATE, INSERT, DELETE
-AS  
+FOR UPDATE, INSERT, DELETE
+AS
 IF (ROWCOUNT_BIG() = 0)
 RETURN;
 BEGIN
+	DECLARE @Operation VARCHAR(6) = 
+    CASE WHEN EXISTS(SELECT * FROM inserted) AND EXISTS(SELECT * FROM deleted) 
+        THEN 'Update'
+    WHEN EXISTS(SELECT * FROM inserted) 
+        THEN 'Insert'
+    -- Currently doesn't work for delete, not sure why.
+    --WHEN EXISTS(SELECT * FROM deleted)
+    --    THEN 'Delete'
+    ELSE 
+        NULL -- Unknown operation.
+    END;
 	INSERT INTO UsersAudit
-	(Id, Email, Password, Role, UpdatedBy, UpdatedOn)
-	SELECT i.Id, i.Email, i.Password, i.Role, SUSER_SNAME(), getdate()
+	(Id, Email, Password, Role, UpdatedBy, UpdatedOn, ActionType)
+	SELECT i.Id, i.Email, i.Password, i.Role, SUSER_SNAME(), getdate(), @Operation
 	FROM dbo.Users t
 	inner join inserted i on t.Id = i.Id
 END;
