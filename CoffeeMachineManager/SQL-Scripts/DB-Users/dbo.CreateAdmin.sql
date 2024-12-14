e00af896d09016aa40968df995ce0adf3c8385d8
Amir Jawad <amirjawad79@gmail.com>
2024-12-14 16:16:20 +0100

    Imported SQL files from PreviewMananagmentKlee

---
+++ CoffeeMachineManager/SQL-Scripts/DB-Users/dbo.CreateAdmin.sql
@@ -0,0 +1,17 @@
+USE master;
+
+-- Creating a new login for user,
+-- creating a new role with execute and insert rights,
+-- then finally assigning that role to the user.
+
+CREATE LOGIN camilla WITH PASSWORD = 'tempPWD123';
+
+USE CoffeeMachineManager;
+
+CREATE ROLE dbadmin;
+GRANT SELECT ON Users TO dbadmin;
+GRANT EXECUTE ON Users TO dbadmin;
+GRANT INSERT ON Users TO dbadmin;
+GRANT SELECT ON UsersAudit TO dbadmin;
+CREATE USER camilla FOR LOGIN camilla;
+ALTER ROLE dbadmin ADD MEMBER camilla;
