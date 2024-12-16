6c4bf3b61df085bdf41b7eabeaf8434af69a8a7b
adiliqbal96 <adiliqbal9606@gmail.com>
2024-12-16 11:07:09 +0100

    succesfully added different login-dashboards depending on role

--- CoffeeMachineManager/Pages/Feedback.cshtml.cs
+++ CoffeeMachineManager/Pages/Feedback.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CoffeeMachineManager.Pages
 {
-    [Authorize]
+    
     public class FeedbackModel : PageModel
     {
         public void OnGet()
